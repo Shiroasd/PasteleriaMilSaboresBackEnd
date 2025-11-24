1f8c5d662a0b52251ad9e9d07a76593573d7d2c3
Gaspar <gas.toro@duocuc.cl>
2025-11-24 17:21:22 -0300

    add supabaseservicerolekey

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,3 @@
 const { createClient } = require('@supabase/supabase-js');
-
-// SOLO estas dos variables:
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-
 module.exports = { supabase };
